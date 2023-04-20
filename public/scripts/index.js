2a1a015fe0ff3882bfb83a899c68fe1f8508a165
yufajjiru-dev <luthfi.yufajjiru@efishery.com>
2023-04-20 08:06:46 +0700

    feat(event): listener algorithm

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -1,10 +1,33 @@
 import {Encrypt} from "./libs/encryption/encrypt.js"
 
-function writeToDOM() {
-    var text = document.getElementById("inputText").value;
+let inCharge = false
+
+let updateTrigger = async (prom) => {
+    inCharge = true
+    prom.then(() => {
+    var text = document.getElementById("inputText").value
     let res = Encrypt(text)
     document.getElementById("output").innerHTML = res
-    
+    inCharge = false
+    })
 }
 
-document.getElementById("inputText").addEventListener("keyup", writeToDOM);
+function writeToDOM(event) {
+    var text = document.getElementById("inputText").value
+    let res = Encrypt(text)
+    switch (event.type) {
+        case "keyup":
+            if (!inCharge){
+                let prom = new Promise((resolve) => setTimeout(resolve, 1500))
+                updateTrigger(prom)
+            }
+        break
+        case "submit":
+            event.preventDefault()
+            document.getElementById("output").innerHTML = res
+        break
+    }
+}
+
+document.getElementById("inputText").addEventListener("keyup", writeToDOM)
+document.getElementById("inputForm").addEventListener("submit", writeToDOM)
